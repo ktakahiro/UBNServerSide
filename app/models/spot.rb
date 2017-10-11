1bbb16dd9d0bcfec11acfed490c3c8769d19d35e
ktakahiro <takahiroinjp@gmail.com>
2017-10-11 23:27:58 +0900

    database added

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,18 @@
 class Spot < ApplicationRecord
+  has_many :spot_areas
+  has_many :areas , through: :spot_areas
+
+  has_many :spot_tags
+  has_many :tags , through: :spot_tags
+
+  validates :spot_name, presence: true
+  validates :image_url, presence: true
+  validates :longtitude, presence: true
+  validates :latitude, presence: true
+  validates :is_male, inclusion: { in: [true, false] }
+  validates :is_female, inclusion: { in: [true, false] }
+  validates :start_hour, presence: true
+  validates :end_hour, presence: true
+
+  def 
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,7 @@
 class Tag < ApplicationRecord
+  has_many :spot_tags
+  has_many :spots , through: :spot_tags
+
+  validates :name, presence: true
+
 end
