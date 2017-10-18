73022b760bb150aa4c1dd06763eb422eca0abbed
ktakahiro <takahiroinjp@gmail.com>
2017-10-18 19:15:02 +0900

    test data added(rake db:seed_fu)

---
+++ db/fixtures/04_spot_tag.rb
@@ -0,0 +1,29 @@
+SpotTag.create do |st|
+  st.spot_id = 1
+  st.tag_id = 1
+end
+
+SpotTag.create do |st|
+  st.spot_id = 2
+  st.tag_id = 1
+end
+
+SpotTag.create do |st|
+  st.spot_id = 2
+  st.tag_id = 2
+end
+
+SpotTag.create do |st|
+  st.spot_id = 3
+  st.tag_id = 2
+end
+
+SpotTag.create do |st|
+  st.spot_id = 3
+  st.tag_id = 3
+end
+
+SpotTag.create do |st|
+  st.spot_id = 4
+  st.tag_id = 3
+end
