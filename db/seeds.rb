598dd2965febb007b7cb417d76afbc948306818c
ktakahiro <takahiroinjp@gmail.com>
2017-11-13 19:24:32 +0900

    having problem with spots database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,90 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+Dir.glob("#{Rails.root}/db/seeds/areas.yml").each do |yaml_filename|
+  # yamlのファイル名から、対応するモデルクラスを特定し、クラスをロードする
+  # 下記行が無いと、「ArgumentError: undefined class/module」が発生する。
+  File.basename(yaml_filename,".yml").classify.constantize
+  # すでに登録されているデータを全県削除
+  Area.delete_all
 
+  # yamlに記述されたレコードをDBに登録する。
+  File.open(yaml_filename) do |load_target_yaml|
+    records = YAML.load(load_target_yaml)
+
+    records.each do |record|
+      Area.create(record.attributes)
+    end
+  end
+end
+
+
+=begin
+Dir.glob("#{Rails.root}/db/seeds/spots.yml").each do |yaml_filename|
+  # yamlのファイル名から、対応するモデルクラスを特定し、クラスをロードする
+  # 下記行が無いと、「ArgumentError: undefined class/module」が発生する。
+  File.basename(yaml_filename,".yml").classify.constantize
+  # すでに登録されているデータを全県削除
+  Spot.delete_all
+
+  # yamlに記述されたレコードをDBに登録する。
+  File.open(yaml_filename) do |load_target_yaml|
+    records = YAML.load(load_target_yaml)
+
+    records.each do |record|
+      Spot.create(record.attributes)
+    end
+  end
+end
+=end
+
+Dir.glob("#{Rails.root}/db/seeds/tags.yml").each do |yaml_filename|
+  # yamlのファイル名から、対応するモデルクラスを特定し、クラスをロードする
+  # 下記行が無いと、「ArgumentError: undefined class/module」が発生する。
+  File.basename(yaml_filename,".yml").classify.constantize
+  # すでに登録されているデータを全県削除
+  Tag.delete_all
+
+  # yamlに記述されたレコードをDBに登録する。
+  File.open(yaml_filename) do |load_target_yaml|
+    records = YAML.load(load_target_yaml)
+
+    records.each do |record|
+      Tag.create(record.attributes)
+    end
+  end
+end
+
+Dir.glob("#{Rails.root}/db/seeds/spot_areas.yml").each do |yaml_filename|
+  # yamlのファイル名から、対応するモデルクラスを特定し、クラスをロードする
+  # 下記行が無いと、「ArgumentError: undefined class/module」が発生する。
+  File.basename(yaml_filename,".yml").classify.constantize
+  # すでに登録されているデータを全県削除
+  SpotArea.delete_all
+
+  # yamlに記述されたレコードをDBに登録する。
+  File.open(yaml_filename) do |load_target_yaml|
+    records = YAML.load(load_target_yaml)
+
+    records.each do |record|
+      SpotArea.create(record.attributes)
+    end
+  end
+end
+
+Dir.glob("#{Rails.root}/db/seeds/spot_tags.yml").each do |yaml_filename|
+  # yamlのファイル名から、対応するモデルクラスを特定し、クラスをロードする
+  # 下記行が無いと、「ArgumentError: undefined class/module」が発生する。
+  File.basename(yaml_filename,".yml").classify.constantize
+  # すでに登録されているデータを全県削除
+  SpotTag.delete_all
+
+  # yamlに記述されたレコードをDBに登録する。
+  File.open(yaml_filename) do |load_target_yaml|
+    records = YAML.load(load_target_yaml)
+
+    records.each do |record|
+      SpotTag.create(record.attributes)
+    end
+  end
+end
